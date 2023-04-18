881010d4913cf0bb982fa6f99668493a417faa80
YUSUFPELTEK <yusufpeltek0@gmail.com>
2023-04-19 02:54:05 +0300

    Initial Commit

---
+++ data-based login screen/data_based_login_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  data_based_login_screenApp.swift
+//  data-based login screen
+//
+//  Created by YUSUF on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct data_based_login_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
